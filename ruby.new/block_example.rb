41673805ff01b85f88340195be03c4bbd747d23e
vy.tien <tienminhvydev@gmail.com>
2025-06-24 09:19:49 +0700

    Learn some special syntax

--- ruby.new/block_example.rb
+++ ruby.new/block_example.rb
@@ -25,3 +25,11 @@ animals = %w[ant bee cow sheep dog cat python snake koala]
 animals.each do |animal|
   puts animal
 end
+
+puts '\n'
+
+%w[cat dog horse].each { |name| print name, ' ' }
+5.times { print '*' }
+3.upto(6) { |i| print i }
+('a'..'e').each { |char| print char }
+('a'..'e').each { print _1 }
