82a456e0a45d2be86fe11674ba37c0fbc436e147
vy.tien <tienminhvydev@gmail.com>
2025-06-24 09:41:57 +0700

    OOP ruby

---
+++ oop/bookstore/book.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# a simple class which represents a book
+class BookInStore
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price
+  end
+end
+
+a_book = BookInStore.new('x', 3)
+another_book = BookInStore.new('y', 4)
+p a_book, another_book
