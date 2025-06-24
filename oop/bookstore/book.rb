03ae750fcbb277d3f7410875fad2fe42aa0acc06
vy.tien <tienminhvydev@gmail.com>
2025-06-24 09:47:07 +0700

    Implement to_s (to string) method

--- oop/bookstore/book.rb
+++ oop/bookstore/book.rb
@@ -6,8 +6,14 @@ class BookInStore
     @isbn = isbn
     @price = price
   end
+
+  def to_s
+    "isbn: #{@isbn}, price: #{@price}"
+  end
 end
 
 a_book = BookInStore.new('x', 3)
 another_book = BookInStore.new('y', 4)
 p a_book, another_book
+
+puts a_book, another_book
