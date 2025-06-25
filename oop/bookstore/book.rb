42e43e1e4a7f982ee81bc2766da6d9cb649a2a6a
vy.tien <tienminhvydev@gmail.com>
2025-06-25 08:47:08 +0700

    getter & setter in a nutshell

--- oop/bookstore/book.rb
+++ oop/bookstore/book.rb
@@ -4,7 +4,7 @@
 class BookInStore
   def initialize(isbn, price)
     @isbn = isbn
-    @price = price
+    @price = Float(price)
   end
 
   def to_s
@@ -19,7 +19,20 @@ class BookInStore
   #   @price
   # end
 
-  attr_reader :isbn, :price
+  attr_accessor :price
+  attr_reader :isbn
+
+  # def price=(price)
+  #   @price=price
+  # end ==> attr_writer or attr_accessor
+
+  def price_in_cents
+    (price * 100).round
+  end
+
+  def price_in_cents=(cents)
+    @price = cents / 100.0
+  end
 end
 
 a_book = BookInStore.new('x', 3)
@@ -27,3 +40,18 @@ another_book = BookInStore.new('y', 4)
 p a_book, another_book
 
 puts a_book, another_book
+
+book = BookInStore.new('isbn1', 33.80)
+
+puts "ISBN = #{book.isbn}"
+puts "Price = #{book.price}"
+book.price = book.price * 0.75 # discount price
+puts "New price = #{book.price}"
+
+puts "Price in cents = #{book.price_in_cents}"
+
+puts
+
+book.price_in_cents = 1234
+puts "Price = #{book.price}"
+puts "Price in cents = #{book.price_in_cents}"
