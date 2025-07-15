2d4f0a2751692b7410b749bbfabaa206820ad986
Vy Tien <tienminhvydev@gmail.com>
2025-07-15 15:32:41 +0700

    Yielding with file handling procedure

--- ruby.new/yielding.rb
+++ ruby.new/yielding.rb
@@ -6,3 +6,8 @@ def a_function_with_yields
 end
 
 a_function_with_yields { puts 'meowww' }
+
+f = File.open("#{Dir.pwd}/data/lorem_ipsum.txt")
+f.each.with_index do |line, index|
+  puts "Line #{index + 1} is: #{line}"
+end
