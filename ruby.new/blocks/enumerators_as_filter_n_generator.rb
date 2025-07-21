2b7997a3521265e6bb0d5ed3f292d6a21a5b8ae1
Vy Tien <tienminhvydev@gmail.com>
2025-07-21 16:07:54 +0700

    generator using enumerator

---
+++ ruby.new/blocks/enumerators_as_filter_n_generator.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# A generator algorithm using enumerator
+double_numbers = Enumerator.new do |yielder|
+  num = 1
+  loop do
+    yielder.yield(num)
+    num *= 2
+  end
+end
+
+16.times { print double_numbers.next, ' ' }
+
+# A shorter version of infinite generator
+
+double_numbers = Enumerator.produce(1) do |number|
+  number * 2
+end
+
+puts "\nShorter algorithm but still return the same answer: "
+16.times { print double_numbers.next, ' ' }
