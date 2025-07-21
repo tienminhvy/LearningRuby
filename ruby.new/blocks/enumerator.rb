9c4c2a01b905190ad263646e4b07df6f2eb86464
Vy Tien <tienminhvydev@gmail.com>
2025-07-21 08:41:46 +0700

    enumerators

---
+++ ruby.new/blocks/enumerator.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+a = [15, 26, 'something']
+enum_a = a.to_enum
+# or enum_a = a.each
+p enum_a.next
+p enum_a.next
+p enum_a
+
+h = {
+  dog: 'lucy',
+  fox: 'foxie'
+}
+enum_h = h.to_enum
+p enum_h.next
+p enum_h.next
+
+# reversed enumerator
+
+enum_a = a.to_enum(:reverse_each)
+p enum_a.next
+p enum_a.next
+
+# looping through enumerator
+
+short_enum = [1, 2, 3, 4, 5].to_enum
+long_enum = ('a'..'z').to_enum
+
+p long_enum
+
+loop do
+  puts "#{short_enum.next} - #{long_enum.next}"
+end
